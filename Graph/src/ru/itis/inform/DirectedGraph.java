8a598f5a4cd6f658dc24a1ec5c872611b961a0b3
dxmtrphnlf <dxmtrphn@gmail.com>
2016-02-21 16:03:42 +0300

    a
    a

--- Graph/src/ru/itis/inform/DirectedGraph.java
+++ Graph/src/ru/itis/inform/DirectedGraph.java
@@ -1,8 +1,5 @@
 package ru.itis.inform;
 
-/**
- * Created by Sodium on 17.02.2016.
- */
 public interface DirectedGraph {
     void addDirectedVertex();
     void addDirectedEdge(int vertexA, int vertexB, int Weight);
